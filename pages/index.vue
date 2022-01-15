d1f73d28bb6741886fa76c8f760c3fdb93736de1
Aqilah <aqilahfatin01@gmail.com>
2022-01-15 14:25:20 +0800

    add function get location and and get current weather by location

--- pages/index.vue
+++ pages/index.vue
@@ -5,5 +5,43 @@
 <script>
 export default {
   name: "Index",
+  data() {
+    return {
+      city: "London",
+      API_KEY: "002ce871fae3c25336c2496ff340254c",
+    };
+  },
+
+  methods: {
+    getCurrentLocation() {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.getWeatherByCoords(
+          position.coords.latitude,
+          position.coords.longitude
+        );
+      }),
+        (err) => {
+          console.log(err);
+        };
+    },
+    getWeatherByCity() {
+      this.$axios
+        .$get(`/data/2.5/weather?q=${this.city}&appid=${this.API_KEY}`)
+        .then((response) => {
+          console.log(response);
+        });
+    },
+    getWeatherByCoords(lat, long) {
+      this.$axios
+        .$get(`/data/2.5/weather?lat=${lat}&lon=${long}&appid=${this.API_KEY}`)
+        .then((response) => {
+          console.log(response);
+        });
+    },
+  },
+
+  created() {
+    this.getCurrentLocation();
+  },
 };
 </script>
