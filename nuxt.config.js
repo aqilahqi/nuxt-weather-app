4bdfad9314d7bbdde2a3c042313386e0507a314e
Aqilah Fatin <aqilahfatin01@gmail.com>
2022-11-24 15:54:30 +0800

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: 'Teleport - assessment',
+    titleTemplate: 'Weather App',
     title: 'Teleport',
     htmlAttrs: {
       lang: 'en'
